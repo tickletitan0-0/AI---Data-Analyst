cfaa1a7905e4e3000aeb77d2a00e394977505816
TickleTitan <tickletitan.dev@gmail.com>
2026-06-17 18:03:03 +0530

    Analysis & gropuing

--- src/model.py
+++ src/model.py
@@ -5,7 +5,8 @@ x = df.loc[df["Sales"].idxmax()]
 z = df.loc[df["Sales"].idxmin()]
 y = x["Product"]
 p = z["Product"]
-print("Total sales: ", df["Sales"].sum())
-print("Average sales: ", df["Sales"].mean())
-print("Best product: ",y)
-print("Worst product: ",p)
+
+print(df[df["Sales"]>300])
+print(df[df["Sales"]<400])
+
+print(df.groupby("Category")["Sales"].sum())
